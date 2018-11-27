a51ee10378346c050e5092385b715f5198722fe2
doher <alexandr.pletezhov@gmail.com>
2018-11-27 11:52:26 +0300

    Update questionary.js
    Changes were made to age, gender and retired

--- questionary.js
+++ questionary.js
@@ -9,54 +9,32 @@ var gender;
 var isRetired;
 
 while (!surname) {
-    surname = prompt("Enter your surname:");
+    surname = prompt("Enter your surname:").trim();
 }
 
 while (!firstName) {
-    firstName = prompt("Enter your first name:");
+    firstName = prompt("Enter your first name:").trim();
 }
 
 while (!fatherName) {
-    fatherName = prompt("Enter your father's name:");
+    fatherName = prompt("Enter your father's name:").trim();
 }
 
 while (!isFinite(age) || age < 0) {
-    age = prompt("Enter your age:");
+    age = +prompt("Enter your age:");
 }
 
 ageInDays = age * 365;
-ageInFuture = 1*age + 5;
+ageInFuture = age + 5;
 
 genderMale = confirm("Are you male?");
 
-switch (genderMale) {
-    case true:
-        gender = "male";
-        break;
+gender = genderMale ? "male" : "female";
 
-    case false:
-        gender = "female";
-        break;        
-
-    default:
-        gender = "is not defined";
-        break;
-}
-
-if (gender == "male") {
-    if (age >= 60) {
-        isRetired = "yes";
-    } else {
-        isRetired = "no";
-    }
-} else if (gender == "female") {
-    if (age >= 55) {
-        isRetired = "yes";
-    } else {
-        isRetired = "no";
-    }
+if ((gender == "male" && age >= 60) || (gender == "female" && age >= 55)) {
+    isRetired = "yes";
 } else {
-    isRetired = "is not defined";
+    isRetired = "no";
 }
 
 alert("Your full name: " + surname + ' ' + firstName + ' ' + fatherName + 
